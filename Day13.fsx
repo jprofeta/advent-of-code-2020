3d0429629f6515720b759432f93b94709c095651
Joseph Profeta <joseph.profeta@xyleminc.com>
2021-01-04 16:42:07 -0500

    Add base files for Day 14
    - Skipping Part 2 of day 13 for now

--- Day13.fsx
+++ Day13.fsx
@@ -1,5 +1,5 @@
-printfn "Advent of Code 2020"
-printfn "Day 13: Suttle Search"
+﻿printfn "Advent of Code 2020"
+printfn "Day 13: Shuttle Search"
 printfn ""
 
 open System
@@ -13,8 +13,8 @@ let part1 (schedule : string) =
     let arrivalTime = lines.[0] |> int
     let busses = lines.[1].Split(',') |> Seq.where (fun x -> not (x = "x")) |> Seq.map int |> Seq.toList
 
-    let (bestBus, waitTime) = 
-        busses 
+    let (bestBus, waitTime) =
+        busses
         |> Seq.map (fun b -> (b, b - (arrivalTime % b) ) )
         |> Seq.minBy (fun (b,x) -> x)
     printfn "Next departing bus: %d" bestBus
@@ -22,14 +22,14 @@ let part1 (schedule : string) =
     printfn "Result: %d" (bestBus * waitTime)
     ()
 
-let part2 (schedule : string) = 
+let part2 (schedule : string) =
     let lines = schedule.Split('\n')
     //let arrivalTime = lines.[0] |> int    // Ignored for this part
     let busses = lines.[1].Split(',') |> Seq.map (fun b -> if b = "x" then None else Some(int b) ) |> Seq.toList
     let maxBus = busses |> Seq.maxBy (fun x -> if x.IsNone then 0 else x.Value) |> Option.get |> int64
     let maxBusIdx = busses |> Seq.findIndex (fun b -> b = Some(maxBus |> int) )
     let busOffset = busses |> Seq.mapi (fun i _ -> i - maxBusIdx)
-    
+
     let rec findTime (factor : int64) : int64*int64 =
         let t = maxBus * factor
         let tBusses =
@@ -72,6 +72,27 @@ let mutable j = 1
 for i in 1..10 do
     while ((5*j + 1) % 3 <> 0 || (5*j + 2) % 7 <> 0) do
         j <- j + 1
-    printfn "Found occurance at %d (5*%d)" (5*j) j
+    printfn "Found occurrence at %d (5*%d)" (5*j) j
     j <- j + 1
 
+part2 "0\n7,5,3"
+part2 "0\n3,5,7"
+
+part2 "0\n5,x,3"
+part2 "0\n7,x,5,3"
+
+[1; 2; 3] |> Seq.zip [3; 5; 7] |> Seq.map (fun (a,b) -> a * b)
+
+
+let getInv (a) (b) (d) =
+    // a^-1 → a*a^-1 = d mod b
+    let rec getInv' a b d a' =
+        if b = 1 || ((a'*a) % b) = d then
+            a'
+        else
+            getInv' a b d (a' + 1)
+    getInv' a b d 1
+let modInv a b = getInv a b 1
+
+let foo (x : int[]) =
+    x |> Seq.pairwise |> Seq.map (fun (x,y) -> modInv y x) |> Seq.toArray
---
+++ Day14.fsx
@@ -0,0 +1,9 @@
+printfn "Advent of Code 2020"
+printfn "Day 14: Docking Data"
+printfn ""
+
+open System
+open System.Collections.Generic
+open System.Text.RegularExpressions
+
+#load "Day14Input.fsx"; open Input
